dd2acabf69c8989a8404b87377ddd87bae18f165
may-my-tech <39182655+Taksapong@users.noreply.github.com>
2020-09-14 18:00:54 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>TS calendar</title>
+<meta name="description" content="Taksapong" />
+<style type="text/css">
+body {
+	background-color: #FFF;
+}
+body {
+	color: #00F;
+}
+body {
+	font-size: 18px;
+}
+body {
+	font-size: 36px;
+	background-color: #CCC;
+}
+body {
+	color: #093;
+	background-color: #E5E5E5;
+}
+body {
+	font-family: Georgia, "Times New Roman", Times, serif;
+}
+body {
+	font-weight: bold;
+}
+</style>
+<meta name="description" content="Technical Service
+Calendar Plant" />
+</head>
+Technical Service<br />
+Calendar Plant
+<hr align="left" width="915" size="3" />
+<a href="https://calendar.google.com/calendar/embed?src=l4514s646c4757uptq9qrlonhg%40group.calendar.google.com&ctz=Asia%2FBangkok" target="new"><img src="ALL.jpg" width="915" height="250" alt="ALL" /></a>
+<p><a href="https://calendar.google.com/calendar/embed?src=nw.ckdthai%40gmail.com&ctz=Asia%2FBangkok" target="new"><img src="NW.jpg" width="300" height="380" alt="NC" /></a>
+  <a href="https://calendar.google.com/calendar/embed?src=tc.ckdthai%40gmail.com&ctz=Asia%2FBangkok" target="new"><img src="TC.jpg" width="300" height="380" alt="TC" /></a>
+  <a href="https://calendar.google.com/calendar/embed?src=np.ckdthai%40gmail.com&ctz=Asia%2FBangkok" target="new"><img src="NP.jpg" width="300" height="380" alt="NS" /></a>
+</p>
+<p><a href="https://calendar.google.com/calendar/embed?src=taksapong.may%40gmail.com&ctz=Asia%2FBangkok" target="new"><img src="TK.jpg" width="300" height="380" alt="TK" /></a>
+  <a href="https://calendar.google.com/calendar/embed?src=sp.ckdthai%40gmail.com&ctz=Asia%2FBangkok" target="new"><img src="SP.jpg" width="300" height="380" alt="SP" /></a>
+  <a href="https://calendar.google.com/calendar/embed?src=tw.ckdthai%40gmail.com&ctz=Asia%2FBangkok" target="new"><img src="TW.jpg" width="300" height="380" alt="TW" /></a>
+</p>
+</body>
+</html>
